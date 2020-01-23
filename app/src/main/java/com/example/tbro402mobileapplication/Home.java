4a0e0b96c04a0cf602a431ed24fa84fa9e4371c8
Tyler Brown <tyler.brown@trustradius.com>
2020-01-22 18:36:36 -0600

    added action bar

--- app/src/main/java/com/example/tbro402mobileapplication/Home.java
+++ app/src/main/java/com/example/tbro402mobileapplication/Home.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,8 +18,6 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Menu menu = findViewById(R.id.main);
-        onCreateOptionsMenu(menu);
         setContentView(R.layout.home);
         Button b = findViewById(R.id.Start);
         b.setOnClickListener(new View.OnClickListener() {
@@ -32,8 +31,16 @@ public class Home extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==R.id.refresh){
+            return true;
+        }
+        return false;
     }
 }
